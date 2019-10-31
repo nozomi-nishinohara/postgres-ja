672de8f8efd0282d3231bd9d7657412ceefbdea0
nozomi-nishinohara <39890579+nozomi-nishinohara@users.noreply.github.com>
2019-10-31 12:50:41 +0900

    Delete push.sh

--- push.sh
+++
@@ -1,9 +0,0 @@
-TAG=$1
-
-if [ "${TAG}" = "" ]; then
-    echo "TAGを指定して下さい。"
-    exit 1
-fi
-
-docker build -t nozominishinohara/postgres-ja:${TAG} -f ${TAG}/Dockerfile ${TAG}
-docker push nozominishinohara/postgres-ja:${TAG}
