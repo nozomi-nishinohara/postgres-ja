a47ec157d2a1d10c1989e312b52ce07d0ed7b6aa
nozomi_nishiohara <nozomi.nishinohara@belldata.co.jp>
2019-12-10 23:43:35 +0900

    stamp属性取得Function追加

--- 11/original-docker-entrypoint.sh
+++ 11/original-docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mv /docker-entrypoint-initdb_original.d/000.base-command.sh  /docker-entrypoint-initdb.d/000.base-command.sh
+mv /docker-entrypoint-initdb_original.d/*  /docker-entrypoint-initdb.d/
 docker-entrypoint.sh "$@"
